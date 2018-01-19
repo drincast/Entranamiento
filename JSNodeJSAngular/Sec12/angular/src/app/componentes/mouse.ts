064df5f90be1811e15065effce866ff7f7e58d7b
drincastX <drincast@gmail.com>
2018-01-19 17:15:30 -0500

    se realiza paso de los archivos js a ts

--- JSNodeJSAngular/Sec12/angular/src/app/componentes/mouse.ts
+++ JSNodeJSAngular/Sec12/angular/src/app/componentes/mouse.ts
@@ -1,8 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: "tag-mouse",
   templateUrl: "../vistas/mouse.html"
 })
 
-export class MouseComponente{}
+export class MouseComponente{
+  ngOnInit(){
+    setInterval(()=> {
+      // OBJETO CON LAS PROPIEDADES DEL MOUSE
+      var pm = {
+        zona: document.querySelectorAll('#efectoMouse'),
+        figuras: document.querySelectorAll('#efectoMouse figure'),
+        mouseX: 0,
+        mouseY: 0,
+        horizontal: true,
+        vertical: true,
+      };
+
+      // OBJETO CON LOS METODOS DEL MOUSE
+      var mm = {
+        inicioMouse: function(){
+          //console.log(pm.zona[0]);
+          pm.zona["0"].addEventListener("mousemove", mm.movimientoMouse);
+          for (var i = 0; i < pm.figuras.length; i++) {
+            pm.figuras[i].innerHTML = '<img src="assets/img/mouse/plano0' + i + '.png"/>';
+            pm.figuras[""+i].style.zIndex = -i;
+          }
+
+          setTimeout(function(){
+            pm.zona["0"].style.height = pm.figuras[0].childNodes["0"].height + "px";
+          }, 100);
+        },
+
+        movimientoMouse: function(mouse){
+          pm.mouseX = mouse.offsetX;
+          pm.mouseY = mouse.offsetY;
+
+          for (var i = 0; i < pm.figuras.length; i++) {
+            if(pm.horizontal){
+                pm.figuras[""+i].style.left = -pm.mouseX/(i*(100+50)) + "%";
+            }
+
+            if(pm.vertical){
+              pm.figuras[""+i].style.top = -pm.mouseY/(i*(100+50)) + "%";
+            }
+          }
+        }
+      };
+
+      mm.inicioMouse();
+    }, 90);
+  }
+}
--- JSNodeJSAngular/Sec12/angular/src/app/componentes/slide.ts
+++ JSNodeJSAngular/Sec12/angular/src/app/componentes/slide.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+//OnInit sirve para identificar cuando se ha cargado todos los recursos de angular
+import { Component, OnInit } from '@angular/core';
 
 import { ServicioSlide } from '../servicios/slide.servicio';
 
@@ -37,4 +38,114 @@ export class SlideComponente{
       }
     );
   }
+
+  ngOnInit(){
+    //this.saludo();
+    //objeto con propiedades de slide
+
+    setTimeout(() =>{
+      var p = {
+        paginacion: document.querySelectorAll("#paginacion li"),
+        item: 0,
+        cajaSlide: document.querySelectorAll("#slide ul"),
+        animacionSlide: "slide",
+        imgSlide: document.querySelectorAll('#slide ul li'),
+        avanzar: document.querySelectorAll('#slide #avanzar'),
+        retroceder: document.querySelector('#slide #retroceder'),
+        velocidadSlide: 3000,
+        formatearLoop: false
+      }
+
+
+      //objeto con los métodos del slide
+      var m = {
+        inicioSlide: function(){
+          for(var i = 0; i < p.paginacion.length; i++){
+            p.paginacion[i].addEventListener("click", m.paginacionSlide);
+            p.imgSlide[""+i].style.width = (100/p.paginacion.length) + '%';
+          }
+
+          p.avanzar[0].addEventListener("click", m.avanzar);
+          p.retroceder.addEventListener("click", m.retroceder);
+
+          m.intervalo();
+
+          p.cajaSlide[""+0].style.width = (p.paginacion.length*100) + "%";
+        },
+
+        paginacionSlide: function(item){
+          //p.item = item.target.parentNode.getAttribute("data-item")-1;
+          p.item = item.target.parentNode.getAttribute("value")-1;
+
+          m.movimientoSlide(p.item);
+        },
+
+        avanzar: function(){
+          if(p.item === (p.imgSlide.length-1)){
+            p.item = 0;
+          }
+          else{
+            p.item++;
+          }
+
+          m.movimientoSlide(p.item);
+        },
+
+        retroceder: function(){
+          if(p.item === 0){
+            p.item = p.imgSlide.length-1;
+          }
+          else{
+            p.item--;
+          }
+
+          m.movimientoSlide(p.item);
+        },
+
+        movimientoSlide: function(item){
+          p.formatearLoop = true;
+          p.cajaSlide[""+0].style.left = -(item * 100) + "%";
+
+          for(var i = 0; i < p.paginacion.length; i++){
+            p.paginacion[""+i].style.opacity = .5;
+          }
+
+          p.paginacion[""+item].style.opacity = 1;
+
+          if(p.animacionSlide === "slide"){
+            p.cajaSlide[""+0].style.transition = ".7s left ease-in-out";
+          }
+
+          if(p.animacionSlide === "fade"){
+            // for(var i = 0; i < p.paginacion.length; i++){
+            //
+            // }
+            p.imgSlide[""+item].style.opacity = "0";
+            p.imgSlide[""+item].style.transition = ".7s opacity ease-in-out";
+            setTimeout(function(){
+              p.imgSlide[""+item].style.opacity = "1";
+            }, 500);
+
+          }
+        },
+
+        intervalo: function(){
+          setInterval(function(){
+            if(p.formatearLoop){
+              p.formatearLoop = false;
+            }
+            else{
+              m.avanzar();
+            }
+          }, p.velocidadSlide)
+        }
+      }
+
+      m.inicioSlide();
+    }, 70);
+  }
+
+  // saludo(){
+  //   console.log("hola");
+  // }
 }
